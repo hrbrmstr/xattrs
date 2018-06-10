80a7e4f54d62df920ffa34dd26dc31de1f0cc100
Ralf Herold <ralf.herold@gmx.net>
2018-06-10 08:03:51 +0100

    main linux adaptation: mangling of atrtibute names which under linux (only?) require a namespace as prefix

--- R/util.R
+++ R/util.R
@@ -41,3 +41,53 @@ convert_plist <- function(path) {
   }
 
 }
+
+
+# Linux: attributes are prefixed with a namespace, that is, 
+# an attribute has the form: namespace.attribute 
+# Examples: user.mime_type, trusted.md5sum, system.posix_acl_access
+# for xattrs, the interest is primarily to access the user
+# namespace. When a namespace is missing, "user" is used as namespace.
+# https://linux.die.net/man/5/attr
+#
+# Removing the namespace "user" from attribute names used in 
+# parameters or when listing attributes enables to use the same
+# R code across platforms
+
+linux_namespaces <- c("user", "security", "system", "trusted")
+
+#' handle_user_prefix_param
+#' @param stringvector vector of strings with attribute names
+#' @return stringvector adjusted with required namespace (Linux)
+#' @keywords internal
+#' @importFrom utils sessionInfo
+handle_user_prefix_param <- function(stringvector){
+
+  # vectorised function to work on linux where attribute
+  # name are to be prefixed with "user." for parameters 
+  
+  if(grepl("linux", sessionInfo()$platform)) {
+    unname(sapply(stringvector, function(x) 
+      ifelse(grepl(paste0("^(", paste0(linux_namespaces, collapse = "|"), ")[.]"), x), x, paste0("user.", x))))
+  } else {
+    stringvector
+  }
+}
+
+#' handle_user_prefix_return
+#' @param stringvector vector of strings with attribute names
+#' @return stringvector adjusted without namespace (Linux)
+#' @keywords internal
+#' @importFrom utils sessionInfo
+handle_user_prefix_return <- function(stringvector){
+
+  # vectorised function to work on linux where from
+  # attribute names the prefix "user." should be removed
+  
+  if(grepl("linux", sessionInfo()$platform)) {
+    unname(sapply(stringvector, function(x) ifelse(grepl("^user.", x), sub("^user.(.*)$", "\\1", x))))
+  } else {
+    stringvector
+  }
+}
+

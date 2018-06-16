c7dfe8a1be9c722e16227ae7e9ea75803c400d5a
Ralf Herold <ralf.herold@gmx.net>
2018-06-16 15:57:34 +0100

    adding linux support for get_xattr_raw, get_xattr_size

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -69,29 +69,29 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// get_xattr_raw
-RawVector get_xattr_raw(const std::string path, std::string name, bool follow_symlinks);
-RcppExport SEXP _xattrs_get_xattr_raw(SEXP pathSEXP, SEXP nameSEXP, SEXP follow_symlinksSEXP) {
+// rcpp_get_xattr_raw
+RawVector rcpp_get_xattr_raw(const std::string path, std::string name, bool follow_symlinks);
+RcppExport SEXP _xattrs_rcpp_get_xattr_raw(SEXP pathSEXP, SEXP nameSEXP, SEXP follow_symlinksSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< const std::string >::type path(pathSEXP);
     Rcpp::traits::input_parameter< std::string >::type name(nameSEXP);
     Rcpp::traits::input_parameter< bool >::type follow_symlinks(follow_symlinksSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_xattr_raw(path, name, follow_symlinks));
+    rcpp_result_gen = Rcpp::wrap(rcpp_get_xattr_raw(path, name, follow_symlinks));
     return rcpp_result_gen;
 END_RCPP
 }
-// get_xattr_size
-ssize_t get_xattr_size(const std::string path, std::string name, bool follow_symlinks);
-RcppExport SEXP _xattrs_get_xattr_size(SEXP pathSEXP, SEXP nameSEXP, SEXP follow_symlinksSEXP) {
+// rcpp_get_xattr_size
+ssize_t rcpp_get_xattr_size(const std::string path, std::string name, bool follow_symlinks);
+RcppExport SEXP _xattrs_rcpp_get_xattr_size(SEXP pathSEXP, SEXP nameSEXP, SEXP follow_symlinksSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< const std::string >::type path(pathSEXP);
     Rcpp::traits::input_parameter< std::string >::type name(nameSEXP);
     Rcpp::traits::input_parameter< bool >::type follow_symlinks(follow_symlinksSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_xattr_size(path, name, follow_symlinks));
+    rcpp_result_gen = Rcpp::wrap(rcpp_get_xattr_size(path, name, follow_symlinks));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -114,8 +114,8 @@ static const R_CallMethodDef CallEntries[] = {
     {"_xattrs_rcpp_rm_xattr", (DL_FUNC) &_xattrs_rcpp_rm_xattr, 3},
     {"_xattrs_rcpp_list_xattrs", (DL_FUNC) &_xattrs_rcpp_list_xattrs, 2},
     {"_xattrs_rcpp_get_xattr", (DL_FUNC) &_xattrs_rcpp_get_xattr, 3},
-    {"_xattrs_get_xattr_raw", (DL_FUNC) &_xattrs_get_xattr_raw, 3},
-    {"_xattrs_get_xattr_size", (DL_FUNC) &_xattrs_get_xattr_size, 3},
+    {"_xattrs_rcpp_get_xattr_raw", (DL_FUNC) &_xattrs_rcpp_get_xattr_raw, 3},
+    {"_xattrs_rcpp_get_xattr_size", (DL_FUNC) &_xattrs_rcpp_get_xattr_size, 3},
     {"_xattrs_rcpp_get_xattr_df", (DL_FUNC) &_xattrs_rcpp_get_xattr_df, 2},
     {NULL, NULL, 0}
 };
